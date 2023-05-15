ec6f121310a5ab1827c93160178a51ae84da7b6a
vimal.trivedi <vimal.trivedi@tatvasoft.com>
2023-05-15 10:28:47 +0530

    feat: added pre-commit

---
+++ dist/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const DatabaseConfig_1 = require("./Database/DatabaseConfig");
+dotenv_1.default.config();
+(0, DatabaseConfig_1.connectDatabase)();
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+app.get("/", (_req, res) => {
+    return res.send("Express Typescript on Vercel");
+});
+app.get("/ping", (_req, res) => {
+    return res.send("pong 🏓");
+});
+app.listen(port, () => {
+    return console.log(`Server is listening on ${port}`);
+});
+//# sourceMappingURL=index.js.map
